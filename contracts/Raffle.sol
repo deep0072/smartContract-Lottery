d503c01dbf084968e62876e2e8afdc3143d79851
deep0072 <deepakumar50136@gmail.com>
2022-09-25 23:10:41 +0530

    getting random number from chainlink

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,15 +7,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle_NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     // now create entrance fee
 
     uint256 private immutable i_entranceFee;
     address[] private s_players;
 
-    constructor(uint256 entranceFee) {
+    /* Events */
+
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee)
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+    {
         i_entranceFee = entranceFee;
     }
 
@@ -29,9 +37,19 @@ contract Raffle {
         s_players.push(payable(msg.sender));
 
         //emit an event  when sender added in array
+        emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external {
+        //request random number
+        // once we get it do something with it
+        // 2 transaction process
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     //read entrance Fee
     function getEntranceFee() public view returns (uint256) {
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
 };
