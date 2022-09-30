827362d9f2583f8dc027c497cfea071f1a124748
deep0072 <deepakumar50136@gmail.com>
2022-09-30 17:56:09 +0530

    fetch random winner

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -11,6 +11,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle_NotEnoughEthEntered();
+error Raffle_TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2 {
     // now create entrance fee
@@ -20,15 +21,20 @@ contract Raffle is VRFConsumerBaseV2 {
     uint64 private immutable i_subscriptionId;
     // how many confirmations the Chainlink node should wait before responding
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint16 private constant NUM_WORDS = 1;
 
-    /*limit for how much gas to use for the callback request to your contract's fulfillRandomWords()*/
-    uint32 private s_callbackGasLimit;
+    uint32 private immutable i_callbackGasLimit;
     address[] private s_players;
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+    // lottery variable
+    address private s_recentWinner;
 
     /* Events */
 
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed randomWinner);
 
     constructor(
         address vrfCoordinatorV2,
@@ -38,10 +44,10 @@ contract Raffle is VRFConsumerBaseV2 {
         uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
-        i_vrfCoordinator = vrfCoordinator();
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
-        s_callbackGasLimit = callbackGasLimit;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -57,23 +63,39 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RaffleEnter(msg.sender);
     }
 
+    /* this function  request for random word and gives the request id */
     function requestRandomWinner() external {
         //request random number
         // once we get it do something with it
         // 2 transaction process
-        i_vrfCoordinator.requestRandomWords(
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
-            s_subscriptionId, // id that require to fund the request
+            i_subscriptionId, // id that require to fund the request
             REQUEST_CONFIRMATIONS,
-            callbackGasLimit,
-            numWords
+            i_callbackGasLimit, /*limit for how much gas to use for the callback request to your contract's fulfillRandomWords()*/
+            NUM_WORDS // how many random words we want to get
         );
+
+        emit RequestedRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
-        internal
-        override
-    {}
+    // once we get the random words then get random winner
+    function fulfillRandomWords(
+        uint256, /*requestId*/
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length; // get index of winner
+        address payable recent_winner = s_players[indexOfWinner]; // pick actual winner from array
+        (bool success, ) = recent_winner.call({value: address(this).balance})(
+            ""
+        ); // send all lottery or balace of this contract to this winner
+
+        if (!success) {
+            revert Raffle_TransferFailed();
+        }
+        s_recentWinner = recent_winner; // set the address of winner to storage vaubale
+        emit WinnerPicked(recent_winner);
+    }
 
     //read entrance Fee
     function getEntranceFee() public view returns (uint256) {
